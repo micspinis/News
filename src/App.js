bcdc7ed4888efc693bb52900c2297ce90c960965
Miguel Somohano <miguel.spinis@gmail.com>
2020-07-04 22:20:20 -0500

    Agregando consulta de API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './componentes/Header';
 import Formulario from './componentes/Formulario';
 import useSelect from './hooks/useSelect';
@@ -7,6 +7,20 @@ function App() {
 
   // state para definir la categoria y noticias
   const [ categoria, guardarCategoria ] = useState('');
+  const [ noticias, guardarNoticias ] = useState([]);
+
+  useEffect( () => {
+    const consultarAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=d3911c6798fb4d42a95f49c6fc4a658e`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles);
+      
+    }
+    consultarAPI();
+  }, [categoria]);
 
   return (
     <Fragment>
